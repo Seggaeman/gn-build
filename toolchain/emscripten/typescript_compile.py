96f365c4078559722250b721ddd14bb35f8092f7
Seggaeman <vr.naga@hotmail.com>
2023-11-06 00:43:10 -0500

    Added webpack support

--- toolchain/emscripten/typescript_compile.py
+++ toolchain/emscripten/typescript_compile.py
@@ -1,8 +1,13 @@
+import os
 import sys
 import subprocess
 
 def main()->None:
-    result:int = subprocess.call(sys.argv[1:])
+    #The last portion of the path supplied by GN doesn't exist. Get rid of it.
+    last_slash_pos:int = sys.argv[1].rfind("/");
+    dir_path:str = sys.argv[1][:last_slash_pos]
+    os.chdir(dir_path) #Change execution directory to the one that contains webpack.config.js
+    result:int = subprocess.call(sys.argv[2:])
     return result
 
 if __name__ == "__main__":
