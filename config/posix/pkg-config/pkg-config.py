b678e7c6a1c79afb4cb717910358446551d21807
Seggaeman <vr.naga@hotmail.com>
2026-01-19 20:46:51 -0500

    Added pkg-config support for gtk4 and gtkmm-4.0

---
+++ config/posix/pkg-config/pkg-config.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import json
+import subprocess
+import sys
+
+def main():
+  pkg_name = sys.argv[1]
+  command = [ "pkg-config", "--libs", "--cflags", pkg_name]
+  result = subprocess.run(command, capture_output=True, text=True)
+  flags = result.stdout.split()
+  cflags = []
+  libs = []
+
+  for flag in flags:
+    if flag.startswith("-l"):
+      libs.append(flag[2:])
+    else:
+      cflags.append(flag)
+
+  result = json.dumps({ 
+    "cflags": cflags, 
+    "libs": libs
+  })
+  print(result)
+
+if __name__ == "__main__":
+  main()
